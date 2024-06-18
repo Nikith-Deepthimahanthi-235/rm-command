ab36091b2afbbf7e3d5e54ff6123e96636ea4538
Nikith-Deepthimahanthi-235 <68648413+Nikith-Deepthimahanthi-235@users.noreply.github.com>
2024-06-19 03:00:36 +0530

    app.js

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-
 const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs');
-
+const chalk= require('chalk')
 yargs.command({
     command : 'remove',
     describe : 'path to be removed',
     builder : {
-        url : {
+        path : {
             describe : 'url is here',
             demandOption : true ,
             type : 'string'
         },
-        rOption : {
+        r : {
             describe : 'option to remove contents recursively',
             demandOption : false ,
             type : 'boolean'
         }
     },
-
     handler(argv) {
-        const targetPath = path.resolve(argv.url)
-        remove(targetPath,argv.rOption)
+        const targetPath = path.resolve(argv.path)
+        remove(targetPath,argv.r)
     }
 })
 const remove = (filePath,recursion) => {
-  console.log(recursion)
   if (!fs.existsSync(filePath)) {
     console.log("brrr");
     return;
@@ -35,16 +32,16 @@ const remove = (filePath,recursion) => {
 
   if (stats.isDirectory()) {
     if (!recursion) {
-      console.log("grrrrr")
-      return
+      console.log(chalk.red.inverse("grr"))
+      return;
     }
     // Recursively remove directory contents
-    fs.readdirSync(filePath).forEach((file) => remove(path.join(filePath, file)));
+    fs.readdirSync(filePath).forEach(file => remove(path.join(filePath, file),recursion));
     fs.rmdirSync(filePath);
   } else {
     // Remove file
     fs.unlinkSync(filePath);
   }
-};
+  console.log(chalk.green.inverse("job done!"))};
 yargs.parse()
 
